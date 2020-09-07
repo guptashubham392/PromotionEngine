c6816949f97ed1fa8a904c14448815f046540173
guptashubham392 <70884967+guptashubham392@users.noreply.github.com>
2020-09-08 04:11:54 +0530

    Added Price calculation logic

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -12,7 +12,61 @@ namespace PromotionEngine
         {
 
         }
+
+        private static int GetTotalPrice(List<Product> products)
+        {
+            int counterofA = 0;
+            int priceofA = 50;
+            int counterofB = 0;
+            int priceofB = 30;
+            int CounterofC = 0;
+            int priceofC = 20;
+            int CounterofD = 0;
+            int priceofD = 15;
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A" || pr.Id == "a")
+                {
+                    counterofA = counterofA + 1;
+                }
+                if (pr.Id == "B" || pr.Id == "b")
+                {
+                    counterofB = counterofB + 1;
+                }
+                if (pr.Id == "C" || pr.Id == "c")
+                {
+                    CounterofC = CounterofC + 1;
+                }
+                if (pr.Id == "D" || pr.Id == "d")
+                {
+                    CounterofD = CounterofD + 1;
+                }
+            }
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+            int totalPriceofCD = 0;
+            if (CounterofC > 0 && CounterofD > 0)
+            {
+                int minValue = 0;
+                if (CounterofC < CounterofD)
+                    minValue = CounterofC;
+                else
+                    minValue = CounterofD;
+                for (int i = 1; i <= minValue; i++)
+                {
+                    totalPriceofCD = totalPriceofCD + 30 * i;
+                    CounterofC--;
+                    CounterofD--;
+                }
+
+            }
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD + totalPriceofCD;
+
+        }
     }
+}
 
     public class Product
     {
